7139641eb3dc71719d64c25e2da317f86e134893
davidvilas <davilasanvilar@gmail.com>
2024-11-10 13:15:18 +0100

    Pagination disabled when loading and icon properties changed

--- front/src/components/Pagination.tsx
+++ front/src/components/Pagination.tsx
@@ -7,18 +7,19 @@ interface PaginationProps {
     pageSize: number;
     setPageSize: (pageSize: number) => void;
     hasNextPage: boolean;
+    isLoading: boolean;
 }
 
 export function Pagination(props: PaginationProps) {
 
-    const { page, setPage, pageSize, setPageSize, hasNextPage } = props;
+    const { page, setPage, pageSize, setPageSize, hasNextPage, isLoading } = props;
 
     return (
         <div className="pagination-box" >
             <div className="pagination-controls">
-                <button onClick={() => setPage(page - 1)} disabled={page === 1}><Icon type="chevron-left" /></button>
+                <button onClick={() => setPage(page - 1)} disabled={isLoading || page === 1}><Icon type="chevron-left" /></button>
                 <span>{`${page}`}</span>
-                <button disabled={!hasNextPage} onClick={() => setPage(page + 1)}><Icon type="chevron-right" /></button>
+                <button disabled={!hasNextPage || isLoading} onClick={() => setPage(page + 1)}><Icon type="chevron-right" /></button>
             </div>
             <select value={pageSize} onChange={(e) => setPageSize(Number(e.target.value))}>
                 <option value={5}>{"5"}</option>
--- front/src/components/icon/icons-svg/ChevronLeft.tsx
+++ front/src/components/icon/icons-svg/ChevronLeft.tsx
@@ -3,5 +3,5 @@ export function ChevronLeft() {
 
     return (
         <svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24" fill="none" stroke="currentColor"
-            stroke-width="2" stroke-linecap="round" stroke-linejoin="round"><path d="m15 18-6-6 6-6" /></svg>)
+            strokeWidth="2" strokeLinecap="round" strokeLinejoin="round"><path d="m15 18-6-6 6-6" /></svg>)
 }
